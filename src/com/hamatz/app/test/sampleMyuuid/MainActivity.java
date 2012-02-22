0813d6231dc502acc9dbe76818c7dce38e7ec787
unknown <0120324@.docomo.docomogr.net>
2012-02-22 23:42:52 +0900

    データ取得の際に利用するサンプルを明示的Intentに変更しました

--- src/com/hamatz/app/test/sampleMyuuid/MainActivity.java
+++ src/com/hamatz/app/test/sampleMyuuid/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
 	
 	private final int REQUEST_CODE = 123;
+	private final int REQUEST_CODE2 = 357;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,6 +23,7 @@ public class MainActivity extends Activity {
         findViewById(R.id.send_button).setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
             	 Intent it = new Intent("com.hamatz.app.myuuid.ACTION_GET_UUID");
+            	 it.setClassName("com.hamatz.app.myuuid","com.hamatz.app.myuuid.IPCActivity");
             	 try{
             		 startActivityForResult(it, REQUEST_CODE);
             	 }catch (android.content.ActivityNotFoundException e){
@@ -36,6 +38,25 @@ public class MainActivity extends Activity {
             	 }
             }
         });
+        
+        findViewById(R.id.check_button).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	 Intent it3 = new Intent("com.hamatz.app.myuuid.ACTION_GET_SYNCANCHOR");
+            	 it3.setClassName("com.hamatz.app.myuuid","com.hamatz.app.myuuid.IPCActivity");
+            	 try{
+            		 startActivityForResult(it3, REQUEST_CODE2);
+            	 }catch (android.content.ActivityNotFoundException e){
+            		 // MyUUIDがインストールされてなかった場合の処理
+            		 Toast.makeText(getApplicationContext(), alertMessage, Toast.LENGTH_SHORT).show();
+            		 
+            		 Uri uri = Uri.parse("market://details?id=com.hamatz.app.myuuid");
+            		 Intent it4 = new Intent();
+            		 it4.setAction(Intent.ACTION_VIEW);
+            		 it4.setData(uri);
+            		 startActivity(it4);
+            	 }
+            }
+        });
     }
     
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
@@ -44,6 +65,8 @@ public class MainActivity extends Activity {
         final String message_none = getText(R.string.msg2).toString();
         final String message_ok1 = getText(R.string.msg3).toString();
         final String message_ok2 = getText(R.string.msg4).toString();
+        final String message_ok3 = getText(R.string.msg5).toString();
+        final String message_ok4 = getText(R.string.msg6).toString();
         
       if (requestCode == REQUEST_CODE)
       {
@@ -62,6 +85,15 @@ public class MainActivity extends Activity {
           TextView text = (TextView) findViewById(R.id.text);
           text.setText(message_ok1 + " " + uuid + " " + message_ok2);
         }
+      }
+      if (requestCode == REQUEST_CODE2)
+      {
+    	  if (resultCode == -1){
+    	  // 返り値がnullでも特にケアしていない点に注意
+    		  String anchor = data.getStringExtra("Anchor");
+    		  TextView text = (TextView) findViewById(R.id.text);
+    		  text.setText(message_ok3 + " " + anchor + " " + message_ok4);
+    	  }
       }
     }
 }
